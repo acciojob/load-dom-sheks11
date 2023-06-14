fc3e4fc7ba17ac1da2d03fa8caec4b5298daeed1
sheks11 <110970118+sheks11@users.noreply.github.com>
2023-06-15 00:28:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 //your JS code here. If required.
 window.addEventListener("DOMContentLoaded", (event) => {
- let ph = document.createElement("p");
-	ph.innerHTML="DOM load success";
-	document.body.appendChild(ph)
+	document.body.innerHTMl="DOM load success";
 });
